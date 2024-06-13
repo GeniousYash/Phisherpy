82cf5e12cb76574500008201951d83d79fa0bb47
Yash Singh <singhyash3012@gmail.com>
2024-06-13 20:33:08 +0530

    Update setup.py

--- files/setup.py
+++ files/setup.py
@@ -6,15 +6,14 @@ version = open("files/version.txt").read().strip()
 long_description = open("README.md").read().strip()
 
 setup(
-    name='MaxPhisher',
+    name='Phisherpy',
     version=version,
     description='A python phishing script for login phishing, image phishing video phishing an many more!',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='KasRoudra',
-    author_email='kasroudrakrd@gmail.com',
+    author='Yash Singh',
     license="MIT",
-    url='https://github.com/KasRoudra/MaxPhisher/',
+    url='https://github.com/GeniousYash/Phisherpy/',
     scripts=['maxphisher'],
     install_requires=["requests", "rich"]
 )
